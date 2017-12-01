2b347460fc924365cbc22c244264b7e3efded32f
Philip Zak <zakphi@gmail.com>
2017-12-01 15:58:48 -0500

    dangerously set html for cardCzar black card

--- client/src/components/CardCzar.jsx
+++ client/src/components/CardCzar.jsx
@@ -1,21 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const CardCzar = (props) => {
+class CardCzar extends Component {
+  renderHTML(){
+    return {__html: this.props.blackCard.text}
+  }
 
+  renderBlackCard(){
+    console.log(this.renderHTML())
+    return <p dangerouslySetInnerHTML={this.renderHTML()}></p>
+  }
+
+  render(){
     return (
       <div className='card-czar'>
         <div className='black-card'>
-          <p>{props.blackCard ? props.blackCard.text : ''}</p>
+          {this.props.blackCard ? this.renderBlackCard() : ''}
         </div>
         <div className='message'>
-          <p>{props.message}</p>
+          <p>{this.props.message}</p>
         </div>
-        {!props.gameStarted ? 
-          <button onClick={props.startGame}>START</button>
+        {!this.props.gameStarted ?
+          <button onClick={this.props.startGame}>START</button>
         : ''}
       </div>
     )
-
+  }
 }
 
 export default CardCzar;
