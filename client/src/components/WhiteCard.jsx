06c55fa50056646c0703435192df4f26b89b572f
Ryan Edwards <ryan.deland.edwards@gmail.com>
2017-12-04 16:04:40 -0500

    phils formatting sucks shit here too

--- client/src/components/WhiteCard.jsx
+++ client/src/components/WhiteCard.jsx
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
  
-class WhiteCard extends Component{
-  renderHTML(){
+class WhiteCard extends Component {
+  renderHTML() {
     return {__html: this.props.text}
   }
 
-  renderWhiteCard(){
-    console.log(this.renderHTML())
+  renderWhiteCard() {
+    //console.log(this.renderHTML())
     return <p dangerouslySetInnerHTML={this.renderHTML()}></p>
   }
 
-  render(){
+  render() {
     return (
       <div className='white-card' onClick={this.props.gameStarted ? () => {this.props.handleCardSelection(this.props.text)} : null}>
         {this.renderWhiteCard()}
