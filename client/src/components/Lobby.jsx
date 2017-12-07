7595cd285c4ed78a9fe8e5d40e032519f440b98a
Ryan Edwards <ryan.deland.edwards@gmail.com>
2017-12-07 15:46:49 -0500

    change Lobby to stateless component

--- client/src/components/Lobby.jsx
+++ client/src/components/Lobby.jsx
@@ -1,26 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Lobby extends Component {
+const Lobby = (props) => {
 
-  renderPlayers() {
-    return this.props.players.map((player) => {
-      return (
-        <p className={'player' + (player.ready ? ' ready' : '')} key={player.id}>{player.name}</p>
-      )
-    })
-  }
-
-  render() {
-    return (
-      <div className='lobby'>
-        <h2>Welcome to</h2>
-        <h1>{this.props.roomCode}</h1>
-        <h3>Horrible People:</h3>
-        {this.renderPlayers()}
-        <button onClick={this.props.readyUp}>Ready</button>
-      </div>
-    )
-  }
+  return (
+    <div className='lobby'>
+      <h2>Welcome to</h2>
+      <h1>{props.roomCode}</h1>
+      <h3>Horrible People:</h3>
+      {props.players.map((player) => {
+        return (
+          <p className={'player' + (player.ready ? ' ready' : '')} key={player.id}>{player.name}</p>
+        )
+      })}
+      <button onClick={props.readyUp}>Ready</button>
+    </div>
+  )
 }
 
 export default Lobby;
