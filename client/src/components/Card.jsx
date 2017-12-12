1b9ed4ecfd2a472c093cb51879bc81338e0008d4
Ryan Edwards <ryan.deland.edwards@gmail.com>
2017-12-12 15:27:52 -0500

    Card component gets a boost, hover exposes pick button

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,21 +1,47 @@
 import React, { Component } from 'react';
  
 class Card extends Component {
+  constructor() {
+    super()
+
+    this.state = ({
+      hovered: false
+    })
+
+    this.handleHover = this.handleHover.bind(this);
+  }
+
+  handleHover(bool) {
+    this.setState({
+      hovered: bool,
+    })
+  }
+
   renderHTML() {
     return {__html: this.props.text}
   }
 
   renderCard() {
-    return <p dangerouslySetInnerHTML={this.renderHTML()}></p>
+    return <p style={{zIndex: this.props.index}} dangerouslySetInnerHTML={this.renderHTML()}></p>
   }
 
   render() {
+    const { hovered } = this.state;
+    const { color, heldBy, gameStarted } = this.props;
     return (
-      <div className={`${this.props.color}-card`} onClick={this.props.gameStarted ? () => {this.props.handleCardSelection(this.props.text)} : null}>
+      <div 
+        className={`${this.props.color}-card`} 
+        style={{zIndex: this.props.index, transform:`translateZ(${this.props.index}px)`}}
+        onMouseEnter={() => this.handleHover(true)}
+        onMouseLeave={() => this.handleHover(false)}
+        >
         {this.renderCard()}
+        {hovered && color === 'white' && heldBy === 'player' && gameStarted && <button onClick={() => this.props.handleCardSelection(this.props.text)}>PICK</button>}
       </div>
     )
   }
 }
 
+        {/* onMouseEnter={this.props.handleCardHover ? () => this.props.handleCardHover(this.props.index) : null}
+        onMouseLeave={this.props.handleCardHover ? () => this.props.handleCardHover(null) : null} */}
 export default Card;
