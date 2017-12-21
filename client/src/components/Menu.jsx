83a10f0d3dd14762d0128edc5c89a2be34106ab6
Ryan Edwards <ryan.deland.edwards@gmail.com>
2017-12-21 16:33:55 -0500

    change order of Menu, add About button

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -26,10 +26,11 @@ class Menu extends Component {
       <div className={`menu ${menuScreen !== '' && 'expanded'}`}>
         { menuScreen === '' &&
           <ul className='menu-list'>
-            {currentScreen !== 'home' && <li onClick={leaveGame}>Leave Game</li>}
-            {roomCode && <li>Room: {roomCode}</li>}
-            {players.length > 0 && <li onClick={() => this.setMenuScreen('scores')}>Scores</li>}
-            {winningCards.length > 0 && <li onClick={() => this.setMenuScreen('winners')}>Winners</li>}
+            {roomCode && <li className='room-code-menu'>Room: {roomCode}</li>}
+            {currentScreen !== 'home' && <li className='leave-game-menu' onClick={leaveGame}>Leave Game.</li>}
+            {players.length > 0 && <li className='scores-menu' onClick={() => this.setMenuScreen('scores')}>Scores.</li>}
+            {winningCards.length > 0 && <li className='winners-menu' onClick={() => this.setMenuScreen('winners')}>Winners.</li>}
+            <li className='about-menu' onClick={() => this.setMenuScreen('about')}>About.</li>
           </ul>
         }
         { menuScreen === 'scores' && <Scores setMenuScreen={this.setMenuScreen} players={players} /> }
