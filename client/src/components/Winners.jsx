44b874b6ee872cee41f20e596de160931c96c435
Ryan Edwards <ryan.deland.edwards@gmail.com>
2017-12-21 13:19:16 -0500

    create new components to display winners and scores

---
+++ client/src/components/Winners.jsx
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+
+import Winner from './Winner';
+
+class Winners extends Component {
+  constructor() {
+    super()
+    this.state = {
+      currentWinner: null,
+      currentWinnerIndex: 0,
+    }
+    this.iterateWinner = this.iterateWinner.bind(this);
+  }
+
+  componentWillMount() {
+    this.setState({
+      currentWinner: this.props.winningCards[0],
+    })
+  }
+
+  iterateWinner(iteration) {
+    this.setState((prevState) => {
+      let currentWinnerIndex;
+      if (prevState.currentWinnerIndex + iteration < 0) {
+        currentWinnerIndex = this.props.winningCards.length - 1;
+      } else if (prevState.currentWinnerIndex + iteration === this.props.winningCards.length) {
+        currentWinnerIndex = 0;
+      } else {
+        currentWinnerIndex = prevState.currentWinnerIndex + iteration;
+      }
+
+      return {
+        currentWinner: this.props.winningCards[currentWinnerIndex],
+        currentWinnerIndex
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div className='winners'>
+        <span className='exit-screen' onClick={() => this.props.setMenuScreen('')}>
+          <i className="far fa-times-circle"></i>
+        </span>
+        <div className='winner-container'>
+          <div className='prev-winner' onClick={() => this.iterateWinner(-1)}>
+            <i className="fas fa-chevron-left"></i>
+          </div>
+          <Winner winner={this.state.currentWinner}/>
+          <div className='next-winner' onClick={() => this.iterateWinner(1)}>
+            <i className="fas fa-chevron-right"></i>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Winners;
