7eae4d13cc428759bc85f0309b160f7e7f68bf81
Ryan Edwards <ryan.deland.edwards@gmail.com>
2017-12-21 13:22:02 -0500

    restructure Menu for Scores and Winners display

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Scores from './Scores';
+import Winners from './Winners';
 
-const Menu = (props) => {
-  return (
-    <div className='menu'>
-      <p onClick={props.leaveGame}>Leave Game</p>
-      {props.roomCode && <p>Room: {props.roomCode}</p>}
-      <p>Scores</p>
-      <p>Winners</p>
-    </div>
-  )
+class Menu extends Component {
+  constructor() {
+    super();
+    this.state = {
+      menuScreen: ''
+    }
+    this.setMenuScreen = this.setMenuScreen.bind(this);
+  }
+
+  setMenuScreen(screen) {
+    this.setState({
+      menuScreen: screen,
+    })
+  }
+
+  render() {
+    const { leaveGame, roomCode, players, winningCards, currentScreen } = this.props;
+    const { menuScreen } = this.state;
+
+    return (
+      <div className={`menu ${menuScreen !== '' && 'expanded'}`}>
+        { menuScreen === '' &&
+          <ul className='menu-list'>
+            {currentScreen !== 'home' && <li onClick={leaveGame}>Leave Game</li>}
+            {roomCode && <li>Room: {roomCode}</li>}
+            {players.length > 0 && <li onClick={() => this.setMenuScreen('scores')}>Scores</li>}
+            {winningCards.length > 0 && <li onClick={() => this.setMenuScreen('winners')}>Winners</li>}
+          </ul>
+        }
+        { menuScreen === 'scores' && <Scores setMenuScreen={this.setMenuScreen} players={players} /> }
+        { menuScreen === 'winners' && <Winners setMenuScreen={this.setMenuScreen} winningCards={winningCards}/> }
+      </div>
+    )
+  }
 }
 
 export default Menu;
