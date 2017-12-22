7f8e79c1a3aac64a0e74ac3a130229728baca5f5
Philip Zak <zakphi@gmail.com>
2017-12-22 15:21:27 -0500

    made home functional component

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,25 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import InputField from './InputField';
 
-class Home extends Component {
-  render() {
-    return (
-      <div className='home'>
-        <InputField
-          value={!this.props.joiningGame ? this.props.name : this.props.roomCode}
-          max={!this.props.joiningGame ? 20 : 5}
-          fieldName={!this.props.joiningGame ? "name" : "roomCode"}
-          placeholder={!this.props.joiningGame ? "Who are you?" : "code"}
-          handleInputChange={this.props.handleInputChange} />
-        {!this.props.joiningGame ? <button onClick={this.props.createGame}>Create</button> : <button onClick={this.props.toggleJoiningGame}>Back</button>}
-        <button onClick={this.props.joiningGame ? this.props.joinGame : () => this.props.toggleJoiningGame()}>Join</button>
-        <div className='message'>
-          {this.props.message}
-        </div>
+const Home = (props) => {
+  return (
+    <div className='home'>
+      <InputField
+        value={!this.props.joiningGame ? this.props.name : this.props.roomCode}
+        max={!this.props.joiningGame ? 20 : 5}
+        fieldName={!this.props.joiningGame ? "name" : "roomCode"}
+        placeholder={!this.props.joiningGame ? "Who are you?" : "code"}
+        handleInputChange={this.props.handleInputChange} />
+      {!this.props.joiningGame ? <button onClick={this.props.createGame}>Create</button> : <button onClick={this.props.toggleJoiningGame}>Back</button>}
+      <button onClick={this.props.joiningGame ? this.props.joinGame : () => this.props.toggleJoiningGame()}>Join</button>
+      <div className='message'>
+        {this.props.message}
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default Home;
