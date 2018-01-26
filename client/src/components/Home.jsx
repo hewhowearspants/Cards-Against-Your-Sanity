33d394bf6205cd62b1e8edbd516d48ad367b6704
Ryan Edwards <ryan.deland.edwards@gmail.com>
2018-01-26 15:42:45 -0500

    restructure Home component for CSS

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -5,18 +5,19 @@ import InputField from './InputField';
 const Home = (props) => {
   return (
     <div className='home'>
-      <InputField
-        value={!props.joiningGame ? props.name : props.roomCode}
-        max={!props.joiningGame ? 20 : 5}
-        fieldName={!props.joiningGame ? "name" : "roomCode"}
-        placeholder={!props.joiningGame ? "Who are you?" : "code"}
-        handleInputChange={props.handleInputChange} />
-      {!props.joiningGame ? <button onClick={props.createGame}>Create</button> : <button onClick={props.toggleJoiningGame}>Back</button>}
-      <button onClick={props.joiningGame ? props.joinGame : () => props.toggleJoiningGame()}>Join</button>
-      <div className='message'>
-        {props.message}
+      <div className='top'>
+        <InputField
+          value={!props.joiningGame ? props.name : props.roomCode}
+          max={!props.joiningGame ? 20 : 5}
+          fieldName={!props.joiningGame ? "name" : "roomCode"}
+          placeholder={!props.joiningGame ? "Who are you?" : "Enter code"}
+          handleInputChange={props.handleInputChange} />
+        {!props.joiningGame ? <button onClick={props.createGame}>Create</button> : <button onClick={props.toggleJoiningGame}>Back</button>}
+        <button onClick={props.joiningGame ? props.joinGame : () => props.toggleJoiningGame()}>Join</button>
       </div>
-      <img className='logo' src='../favicon.png' alt='shitty logo' />
+      <div className='bottom'>
+        <img className='logo' src='../favicon.png' alt='shitty logo' />
+      </div>
     </div>
   )
 }
