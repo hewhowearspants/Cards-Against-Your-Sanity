d0922988a345d030cebeeb4d0f6f8212fe342870
Ryan Edwards <ryan.deland.edwards@gmail.com>
2018-02-01 16:41:35 -0500

    remove numbers from room code generator

--- server.js
+++ server.js
@@ -494,7 +494,7 @@ io.on('connection', (socket) => {
   // generates a random 5-digit alphanumeric room code for players to join
   function roomCodeGen() {
     let roomCode = '';
-    let charBank = 'abcdefghijklmnpqrstuvwxyz123456789';
+    let charBank = 'abcdefghijklmnpqrstuvwxyz';
 
     for(let i = 0; i < 5; i++){
       roomCode += charBank.charAt(Math.floor(Math.random() * charBank.length));
